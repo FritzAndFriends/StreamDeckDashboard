6ea7a44b3df2433fcec84afc3aac6be493f3c501
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2019-01-29 19:58:31 -0500

    First commit

---
+++ UnitTestPlugin.cs
@@ -0,0 +1,30 @@
+﻿using StreamDeckLib;
+using StreamDeckLib.Messages;
+using System.Threading.Tasks;
+
+namespace Fritz.StreamDeckDashboard
+{
+	internal class UnitTestPlugin : BaseStreamDeckPlugin
+	{
+
+		// Cheer 100 Auth0bobby  January 29, 2019
+		// Cheer 100 roberttables  January 29, 2019
+		// Cheer 100 cpayette  January 29, 2019
+
+
+		/**
+		 * 
+		 * Not Running -- Unit Test Button
+		 * Currently Running
+		 * Green button with check -- Success
+		 * Yellow -- success with warnings
+		 * Red button with big X -- failure
+		 */
+
+		public override async Task OnKeyUp(StreamDeckEventPayload args)
+		{
+			// Your plugin code goes here.
+		}
+
+	}
+}
