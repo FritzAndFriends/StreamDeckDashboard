c57006ab321488b69bd6e4534cba68bbf4d46fec
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2019-01-29 20:12:13 -0500

    Added some statuses and initial pseudocode

--- UnitTestPlugin.cs
+++ UnitTestPlugin.cs
@@ -1,5 +1,7 @@
 ﻿using StreamDeckLib;
 using StreamDeckLib.Messages;
+using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace Fritz.StreamDeckDashboard
@@ -11,6 +13,15 @@ namespace Fritz.StreamDeckDashboard
 		// Cheer 100 roberttables  January 29, 2019
 		// Cheer 100 cpayette  January 29, 2019
 
+		public enum UnitTestButtonState {
+			NoTestsRunning,
+			TestsRunning,
+			TestsPassed,
+			TestsPassedWithWarnings,
+			TestsFailed
+		}
+
+		private Stopwatch _ButtonHoldTimer;
 
 		/**
 		 * 
@@ -21,9 +32,64 @@ namespace Fritz.StreamDeckDashboard
 		 * Red button with big X -- failure
 		 */
 
+		public UnitTestButtonState State { get; set; }
+
 		public override async Task OnKeyUp(StreamDeckEventPayload args)
 		{
-			// Your plugin code goes here.
+
+			if (State != UnitTestButtonState.NoTestsRunning && _ButtonHoldTimer.Elapsed.TotalSeconds > 2)
+			{
+				await StopTests();
+				return;
+			} else if (State == UnitTestButtonState.NoTestsRunning) {
+				await StartTests();
+				return;
+			}
+
+			// TODO:  Figure out what to do in each of the other states...
+			// NOTE: If tests have completed, perhaps open a report of those tests
+
+		}
+
+		public override Task OnKeyDown(StreamDeckEventPayload args)
+		{
+			
+			if (State != UnitTestButtonState.NoTestsRunning) {
+				_ButtonHoldTimer = Stopwatch.StartNew();
+			}
+
+			return Task.CompletedTask;
+
+		}
+
+		public override async Task OnWillDisappear(StreamDeckEventPayload args)
+		{
+
+			var currentState = new
+			{
+				State = (int)(this.State)
+			};
+
+			await this.Manager.SetSettingsAsync(args.context, currentState);
+
+		}
+
+		public override Task OnWillAppear(StreamDeckEventPayload args)
+		{
+
+			this.State = args.payload.settings.State;
+
+			return base.OnWillAppear(args);
+		}
+
+		private Task StartTests()
+		{
+			throw new NotImplementedException();
+		}
+
+		private Task StopTests()
+		{
+			throw new NotImplementedException();
 		}
 
 	}
