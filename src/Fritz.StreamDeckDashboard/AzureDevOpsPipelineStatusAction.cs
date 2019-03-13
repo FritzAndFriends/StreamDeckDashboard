0131299759b888cfcce88983499ec5e9d1de716a
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2019-03-12 20:20:05 -0400

    Started work on a DevOps status action

---
+++ src/Fritz.StreamDeckDashboard/AzureDevOpsPipelineStatusAction.cs
@@ -0,0 +1,28 @@
+﻿using StreamDeckLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fritz.StreamDeckDashboard
+{
+
+	[ActionUuid("com.csharpfritz.plugin.azuredevopsstatus.action")]
+	public class AzureDevOpsPipelineStatusAction : BaseStreamDeckActionWithSettingsModel<Models.AzureDevopsPipelineSettings>
+	{
+
+		/*
+		 * Fetch from URL similar to:  https://dev.azure.com/FritzAndFriends/StreamDeckTools/_apis/build/status/StreamDeckTools-CI?api-version=5.1-preview.1
+
+				200 per 5 minute interval
+				Once every 5 seconds
+			*/
+
+		// Cheer 1242 roberttables 12/3/19 
+		// Cheer 142 cpayette 12/3/19 
+
+
+
+
+
+	}
+}
---
+++ src/Fritz.StreamDeckDashboard/Models/AzureDevopsPipelineSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fritz.StreamDeckDashboard.Models
+{
+	public class AzureDevopsPipelineSettings
+	{
+
+		public string OrgName { get; set; }
+
+		public string ProjectName { get; set; }
+
+		public string PipelineName { get; set; }
+
+	}
+}
